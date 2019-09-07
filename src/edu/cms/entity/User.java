f9c901b5ae28c9b1a7fdfe585a866a3928b0f5a3
sususama <1023605039@qq.com>
2019-09-07 20:38:49 +0800

    9/7

---
+++ src/edu/cms/entity/User.java
@@ -0,0 +1,51 @@
+package edu.xalead.cms.entity;
+
+import java.util.Date;
+
+public class User {
+    private int uid;
+    private String username;
+    private String password;
+    private Date createTime;
+    private String uname;
+
+    public int getUid() {
+        return uid;
+    }
+
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getUname() {
+        return uname;
+    }
+
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
+}
